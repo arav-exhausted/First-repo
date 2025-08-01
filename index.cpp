99f31f5acc36cc4c78cddf1cd3856e037a3ac070
arav-exhausted <aravprajapat1102@gmail.com>
2025-08-01 16:45:33 +0530

    index.cpp

--- index.cpp
+++ index.cpp
@@ -1,27 +1,7 @@
 #include <iostream>
 using namespace std;
 
-
-
 int main(){
-    //i represents row and j represent column
-
-    int n;
-    cin>>n;
-    int row =1;
-
-    while(row<=n){
-
-        int col=1;
-        while(col<=n){
-            cout<<"*";
-            col = col +1;
-
-        }
-        cout<<endl;
-
-
-        row = row + 1 ;
-
-    }
+    cout<<"Hello World"<<endl;
+    
 }
